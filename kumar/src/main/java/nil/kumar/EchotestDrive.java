d5e6bfe49102669c2f9a492149d1cb87b4376d81
nilmani <nilmani@nilmani-PC>
2018-07-27 11:52:12 +0530

    test maven

---
+++ kumar/src/main/java/nil/kumar/EchotestDrive.java
@@ -0,0 +1,27 @@
+package nil.kumar;
+
+public class EchotestDrive {
+public static void main(String [] args) {
+Echo e1 = new Echo();
+Echo e2 = new Echo();
+int x = 0;
+while ( x<=4 ) {
+e1.hello();
+System.out.println("helloooo.......");
+if (e2.count<=4 ) {
+e2.count = e2.count + 1;
+}
+if (e2.count<=4 ) {
+e2.count = e2.count + e1.count;
+}
+x = x + 1;
+}
+System.out.println(e2.count);
+}
+}
+class Echo{
+int count = 0;
+void hello() {
+System.out.println("helloooo....... ");
+}
+}
---
+++ kumar/src/test/java/nil/kumar/DrumKitTestDrive.java
@@ -0,0 +1,31 @@
+package nil.kumar;
+class Drumkit{
+boolean topHat = true;
+boolean snare = true;
+
+void playTopHat () {
+System.out.println("ding ding da-ding");
+}
+void playSnare() {
+System.out.println("bang bang ba-bang");
+}
+
+}
+
+public class DrumKitTestDrive {
+public static void main(String [] args) {
+
+Drumkit d = new Drumkit();
+d.playTopHat();
+d.playSnare();
+d.snare = false;
+
+if (d.snare == true) {
+d.playSnare();
+}
+else{
+	System.out.println("snare is false");
+}
+
+}
+}
